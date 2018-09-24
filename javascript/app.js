8841a402b779c239ca5498fa2820a53ffbc4c60f
MarchWaden <max.linz@live.com>
2018-09-24 14:38:32 -0600

    Added hunger/boredom/sleepiness stats which increase over time, added buttons which reduce them, added an image of a tiger which disappears when stats get too high

--- javascript/app.js
+++ javascript/app.js
@@ -6,3 +6,45 @@ class Tomagachi{
     this.boredom = 0;
   }
 }
+let interval;
+let tiger;
+const tick_loop = () => {
+  $('.name').html(`<h5 class="name">${tiger.name}</h5>`);
+  $('.stats').html(`<p class="stats">
+  Hunger: ${tiger.hunger}
+  Boredom: ${tiger.boredom}
+  Sleepiness: ${tiger.sleepiness}
+  </p>`)
+  if(tiger.hunger >= 10 || tiger.sleepiness >= 10 || tiger.boredom >= 10){
+    console.log('Your tiger has died!');
+    $('.pet').parent().html('');
+    clearInterval(interval);
+  }
+  if (Math.random() < .2){
+    tiger.hunger++;
+  }
+  if (Math.random() < .2){
+    tiger.sleepiness++;
+  }
+  if (Math.random() < .8){
+    tiger.boredom++;
+  }
+  console.log('tick')
+}
+const play = () => {
+  tiger = new Tomagachi("Tiger");
+  interval = setInterval(tick_loop,1000);
+}
+$('#Feed_me').on('click',()=>{
+  tiger.hunger = 0;
+  tick_loop();
+})
+$('#Pet_me').on('click',()=>{
+  tiger.boredom = 0;
+  tick_loop();
+})
+$('#lights').on('click',()=>{
+  tiger.sleepiness = 0;
+  tick_loop();
+})
+play();
