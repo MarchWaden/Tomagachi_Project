0890f792ca357cb453178249bbe428194676f0a7
MarchWaden <max.linz@live.com>
2018-09-25 14:26:47 -0600

    Buttons no longer work after death, added animated tiger for adult form

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,4 @@
+has_died = false;
 class Tomagachi{
   constructor(name){
     this.name = name;
@@ -7,10 +8,7 @@ class Tomagachi{
     this.age = 0;
   }
 }
-let interval;
-let tiger;
-const tick_loop = () => {
-  tiger.age++;
+const update_stats = () => {
   $('.name').html(`<h5 class="name">${tiger.name}</h5>`);
   $('.stats').html(`<p class="stats">
   Hunger: ${tiger.hunger}
@@ -18,12 +16,18 @@ const tick_loop = () => {
   Sleepiness: ${tiger.sleepiness}
   Age: ${tiger.age}
   </p>`)
-  if(tiger.age > 10){
-      $('.pet').parent().html('<image class="pet" src="images/adult_tiger.jpg">');
+  if(tiger.age > 10 && !has_died){
+      $('.pet').parent().html('<image class="pet" src="images/animated_tiger.gif">');
   }
+}
+let interval;
+let tiger;
+const tick_loop = () => {
+  tiger.age++;
   if(tiger.hunger >= 10 || tiger.sleepiness >= 10 || tiger.boredom >= 10){
+    has_died = true;
     console.log('Your tiger has died!');
-    $('.pet').parent().html('<image class="pet" src="images/dead_tiger.jpg">');
+    $('.pet').parent().html('<image class="pet" src="images/dead_tiger.jpg"><p>Status: Dead</p>');
     clearInterval(interval);
   }
   if (Math.random() < .2){
@@ -35,21 +39,29 @@ const tick_loop = () => {
   if (Math.random() < .8){
     tiger.boredom++;
   }
+  update_stats();
 }
 const play = () => {
   tiger = new Tomagachi("Tiger");
+  update_stats();
   interval = setInterval(tick_loop,1000);
 }
 $('#Feed_me').on('click',()=>{
+  if(!has_died){
   tiger.hunger = 0;
-  tick_loop();
+  update_stats();
+  }
 })
 $('#Pet_me').on('click',()=>{
+  if(!has_died){
   tiger.boredom = 0;
-  tick_loop();
+  update_stats();
+  }
 })
 $('#lights').on('click',()=>{
+  if(!has_died){
   tiger.sleepiness = 0;
-  tick_loop();
+  update_stats();
+  }
 })
 play();
